6ab314c7c2494891e4e4e4458bf46ca5d9acbbdc
Yogesh-Prasad-Pant <pant444yogesh@gmail.com>
2025-05-30 23:41:23 +0545

    yogesh armstrong nuber

---
+++ armstrong_number.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int n,sum=0,digit,p;
+    printf("Enter any integer number for finding sum of it's digit : ");
+    scanf("%d",&n);
+    p=n;
+    while(n!=0){
+        digit = n%10;
+        sum += digit*digit*digit;
+        n = n/10;
+    }
+    if(sum == p)
+    printf("%d is armstrong number ",sum);
+    else
+    printf("%d is not an armstrong number ",p);
+    getch();
+}
